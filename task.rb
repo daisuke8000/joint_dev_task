0177662ef06786a19a4c5be74d58eb93979fdb95
daisuke8000 <daisuke12108000@gmail.com>
2020-02-27 22:47:18 +0900

    ruby課題 Q1-Q5まで回答

--- task.rb
+++ task.rb
@@ -5,6 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  p names << "斎藤"
 
 end
 
@@ -13,6 +14,7 @@ def q2
   array2 = %w(bird bat tiger)
 
   # 以下に回答を記載
+  p array3 = array1 + array2
 
 end
 
@@ -20,6 +22,7 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  p numbers.count(3)
 
 end
 
@@ -27,6 +30,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  p sports.compact!
 
 end
 
@@ -35,6 +39,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+  p array1.empty?
+  p array2.empty?  
 
 end
 
